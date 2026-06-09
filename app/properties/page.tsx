1a831a07e1ed4df21eb40f63bd6019421a839c04
wyma-dss <waiyanminaung@dss.com.mm>
2026-06-10 01:30:37 +0700

    feat: wrap ListingPage in Suspense for improved loading handling

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { HOME_FOOTER_COLUMNS, HOME_NAV_ITEMS } from "@/app/constants";
 import { HomeFooter, InnerPageHeader } from "@/app/components/home";
 import { ListingPage } from "./listing";
@@ -6,7 +7,9 @@ export default function PropertiesPage() {
   return (
     <>
       <InnerPageHeader navItems={HOME_NAV_ITEMS} />
-      <ListingPage />
+      <Suspense>
+        <ListingPage />
+      </Suspense>
       <HomeFooter columns={HOME_FOOTER_COLUMNS} />
     </>
   );
