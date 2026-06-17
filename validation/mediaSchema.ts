b654bbdb0a0ffd871a44270adc5f6fd1df0027f5
wyma-dss <waiyanminaung@dss.com.mm>
2026-06-17 12:12:44 +0700

    feat: add media validation constants

---
+++ validation/mediaSchema.ts
@@ -0,0 +1,10 @@
+export const ALLOWED_MIME_TYPES = [
+  "image/jpeg",
+  "image/png",
+  "image/webp",
+  "image/gif",
+] as const;
+
+export const MAX_UPLOAD_SIZE = 10 * 1024 * 1024;
+
+export type AllowedMimeType = (typeof ALLOWED_MIME_TYPES)[number];
