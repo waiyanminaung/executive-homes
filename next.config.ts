a8eb09d93f8a3355f502112dc81b587758d455d5
wyma-dss <waiyanminaung@dss.com.mm>
2026-06-22 20:54:42 +0700

    fix(images): add 3840 to deviceSizes to allow large width image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [
       { protocol: "https", hostname: "image.tmdb.org" },
       { protocol: "https", hostname: "images.unsplash.com" },
