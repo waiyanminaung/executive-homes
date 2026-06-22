a51807228cf76aad8464b8a24e3fd46acff0278a
wyma-dss <waiyanminaung@dss.com.mm>
2026-06-22 21:01:32 +0700

    fix(images): add imageSizes config to allow small width optimization

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       { protocol: "https", hostname: "image.tmdb.org" },
       { protocol: "https", hostname: "images.unsplash.com" },
